2f22d4cb660836dfff471c5fbcac0355caf9e31a
Julie Tran <kimtran767@gmail.com>
2019-12-18 19:37:54 -0800

    removed and cleaned up backend user

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,6 @@ class User < ApplicationRecord
     foreign_key: :user_id,
     class_name: :Comment
 
-    # validate :password_complexity #did i use the right method?
-
     after_initialize :ensure_session_token
 
     def self.find_by_credentials(email, password) 
