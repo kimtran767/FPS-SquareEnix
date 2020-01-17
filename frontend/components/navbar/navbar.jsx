4875becdbb4e90981af0cc8a68cc8c9f168b935e
Julie Tran <kimtran767@gmail.com>
2020-01-16 21:07:52 -0800

    cleared navbar state

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import GreetingContainer from '../greeting/greeting_container';
 import { Link } from 'react-router-dom';
 
-
 const NavBar = () => (
   <div className='navbar-header'>
     <Link className='nav-title' to='/'>
